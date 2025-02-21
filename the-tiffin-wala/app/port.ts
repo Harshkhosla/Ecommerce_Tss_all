817e515deb207d510539454de0c86438b70680cb
Harsh Khosla <harshkhosla@Netomis-MacBook-Pro.local>
2025-02-21 19:58:49 +0530

    adding the url for next js

--- the-tiffin-wala/app/port.ts
+++ the-tiffin-wala/app/port.ts
@@ -1,3 +1,6 @@
-export const tssurl = "https://brand.mycarebilling.com/api/client"
+export const tssurl = "https://tiffin.mycarebilling.com/client"
 // export const tssurl = "http://localhost:5200/api/client"
-export const tssurl2 = "https://brand.mycarebilling.com/api/admin"
+export const tssurl2 = "https://tiffin.mycarebilling.com/admin"
+
+
+export const websocket = "https://tiffin.mycarebilling.com/"
